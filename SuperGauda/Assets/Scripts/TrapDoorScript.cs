93683671bb385bb2f397474ed247d2ab74a8c644
Alaere1899 <nibbles1611@gmail.com>
2025-08-21 16:17:14 +0100

    Player 2 falls through stairs and trap doors now
    Jens to fix tomorrow

--- SuperGauda/Assets/Scripts/TrapDoorScript.cs
+++ SuperGauda/Assets/Scripts/TrapDoorScript.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public Transform destination;
+    bool worksForPlayer2 = true;
 
-    public Transform GetDestination()
+    public Transform GetDestination(bool isPlayer2)
     {
+        if(!worksForPlayer2 && isPlayer2) return null;
         return destination;
     }
 }
